52dd8bea6423d8a9841ac3759cf089c42caabd43
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-08-04 23:06:49 +0300

    With Buffalo: Initial Commit

--- customer/src/main/java/ru/iooko/customer/CustomerService.java
+++ customer/src/main/java/ru/iooko/customer/CustomerService.java
@@ -2,12 +2,14 @@ package ru.iooko.customer;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @AllArgsConstructor
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
 
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
@@ -17,7 +19,10 @@ public class CustomerService {
                 .build();
         // todo: check if email valid
         // todo: check if email not taken
-        // todo: store customer in db
+        // todo: check if fraudster
+        restTemplate.getForObject("{http:localhost:8081/api/v1/fraud-check/}",
+                );
         customerRepository.save(customer);
+        // todo: send notification
     }
 }
