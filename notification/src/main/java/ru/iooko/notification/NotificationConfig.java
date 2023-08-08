d702e72516647fd7a9018e5f7d98d742df3a16a8
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-08-09 02:28:21 +0300

    With Buffalo: Queue Topic

--- notification/src/main/java/ru/iooko/notification/NotificationConfig.java
+++ notification/src/main/java/ru/iooko/notification/NotificationConfig.java
@@ -1,7 +1,12 @@
 package ru.iooko.notification;
 
 import lombok.Getter;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -16,4 +21,22 @@ public class NotificationConfig {
 
     @Value("${rabbitmq.routing-keys.internal-notification}")
     private String internalNotificationRoutingKey;
+
+    @Bean
+    public TopicExchange intervalTopicExchange() {
+        return new TopicExchange(this.internalExchange);
+    }
+
+    @Bean
+    public Queue notificationQueue() {
+        return new Queue(this.notificationQueue);
+    }
+
+    @Bean
+    public Binding internalToNotificationBinding() {
+        return BindingBuilder
+                .bind(notificationQueue())
+                .to(intervalTopicExchange())
+                .with(this.internalNotificationRoutingKey);
+    }
 }
