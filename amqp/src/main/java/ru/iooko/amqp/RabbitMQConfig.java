5fd8d8dd522daa222371c8ef1eb7ce09dac31b92
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-08-09 01:34:01 +0300

    With Buffalo: Add SimpleRabbitListenerContainerFactory

--- amqp/src/main/java/ru/iooko/amqp/RabbitMQConfig.java
+++ amqp/src/main/java/ru/iooko/amqp/RabbitMQConfig.java
@@ -2,13 +2,13 @@ package ru.iooko.amqp;
 
 import lombok.AllArgsConstructor;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @Configuration
 @AllArgsConstructor
@@ -21,6 +21,14 @@ public class RabbitMQConfig {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMessageConverter(jacksonConverter());
         return rabbitTemplate;
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory simpleRabbitListenerContainerFactory() {
+        var factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(jacksonConverter());
+        return factory;
     }
 
     @Bean
